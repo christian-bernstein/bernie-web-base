45b37743710a6a8e45c4a98cdf6a0fbc112d5b2b
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2025-02-27 16:34:54 +0100

    unnamed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: "bernie-web-base",
+      fileName: 'bernie-web-base', // .js ?
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    },
+  },
+  plugins: [
+      react()
+  ]
 })
